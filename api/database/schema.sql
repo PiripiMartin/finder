fe52bb9d248ad84b5aab74ab508cdaed4789b3ac
ju-lewis <boollewis04@gmail.com>
2025-08-08 22:37:39 +1000

    Create initial DB schema

---
+++ api/database/schema.sql
@@ -0,0 +1,43 @@
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(32) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password_hash VARCHAR(128) NOT NULL,
+    
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE IF NOT EXISTS user_sessions(
+  session_token CHAR(36) PRIMARY KEY,
+  expires_at DATETIME NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  --ip_address VARCHAR(45), NOTE: not needed right now, can be added in if needed
+  --user_agent TEXT,              ^ same here.
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE IF NOT EXISTS map_points (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    description TEXT,
+    emoji VARCHAR(16) NOT NULL,
+    latitude DECIMAL(9,6) NOT NULL,
+    longitude DECIMAL(9,6) NOT NULL,
+    SPATIAL INDEX idx_location (latitude, longitude),
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE IF NOT EXISTS posts (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    url VARCHAR(2048) NOT NULL,
+    FOREIGN KEY (posted_by) INT REFERENCES users (id),
+
+    posted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,19 +1,8 @@
 
 
-function testRoute(req: Request): Response {
-
-
-    console.log(req);
-
-    return new Response("Hello!");
-}
-
-
-
 Bun.serve({
     port: 8000,
     routes: {
-        "/": testRoute
     }
 });
 
