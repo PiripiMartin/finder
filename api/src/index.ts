a2dfe8426e94199de06818d5508826a07ab1b2e9
ju-lewis <boollewis04@gmail.com>
2025-08-08 20:36:45 +1000

    Template basic Bun server

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello via Bun!");
+
+
+function testRoute(req: Request): Response {
+
+
+    console.log(req);
+
+    return new Response("Hello!");
+}
+
+
+
+Bun.serve({
+    port: 8000,
+    routes: {
+        "/": testRoute
+    }
+});
+
+
+
