fadbb09ee692a873c96d25bef16013dea9cf5e33
ju-lewis <boollewis04@gmail.com>
2025-08-09 12:51:05 +1000

    Implement session verification endpoint

--- api/temp-test.sh
+++ api/temp-test.sh
@@ -1,7 +1,10 @@
 
 # Temporary test API call before proper login page is implemented
 
-curl -X POST http://localhost:8000/api/login \
-  -H "Content-Type: application/json" \
-  -d '{"username": "admin", "password": "test"}'
+#curl -X POST http://localhost:8000/api/login \
+#  -H "Content-Type: application/json" \
+#  -d '{"username": "admin", "password": "test"}'
 
+curl -X POST http://localhost:8000/api/validate-token \
+  -H "Content-Type: application/json" \
+  -d '{"userId": 2, "sessionToken": "396c92fd-f224-470e-ac38-a2f352104c58"}'
