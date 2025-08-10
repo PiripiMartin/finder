7cb78d1e4b93850fd520e495abccc93ce61de1e8
ju-lewis <boollewis04@gmail.com>
2025-08-10 11:27:57 +1000

    Fix variable assignment on DB fetch

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,14 +1,17 @@
 import { db } from "./database";
-import { getPosts } from "./map/routes";
+import { getLocationPosts } from "./map/routes";
 import { login, validateSessionToken } from "./user/routes";
 
 
 Bun.serve({
     port: 8000,
     routes: {
+        // Account management routes
         "/api/login":          {POST: login},
         "/api/validate-token": {GET: validateSessionToken},
-        "/api/map/:id/posts":  {GET: getPosts}
+
+        // Post management routes
+        "/api/map/:id/posts":  {GET: getLocationPosts}
     }
 });
 
