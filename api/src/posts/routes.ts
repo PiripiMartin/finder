15b405c20ed160464dc3f232a8e5a90bac520554
ju-lewis <julianlewis2023@gmail.com>
2025-09-03 01:57:01 +1000

    Continue implementing posting logic

--- api/src/posts/routes.ts
+++ api/src/posts/routes.ts
@@ -1,7 +1,8 @@
 import type { BunRequest } from "bun";
 import { verifySessionToken } from "../user/session";
 import { checkedExtractBody } from "../utils";
-import { extractPossibleLocationName, getTikTokEmbedInfo, searchGooglePlaces } from "./get-location";
+import { extractPossibleLocationName, getGooglePlaceDetails, getTikTokEmbedInfo, searchGooglePlaces } from "./get-location";
+import { db } from "../database";
 
 
 interface NewPostRequest {
@@ -26,16 +27,19 @@ export async function createPost(req: BunRequest): Promise<Response> {
         return new Response("Malformed body", {status: 400});
     }
 
+    // Get TikTok embed info
     const embedInfo = await getTikTokEmbedInfo(data.url);
     if (!embedInfo) {
         return new Response("Error fetching TikTok embed information", {status: 500});
     }
 
+    // Extract possible location name using LLM API
     const possiblePlaceName = await extractPossibleLocationName(embedInfo);
     if (!possiblePlaceName) {
         return new Response("Error extracing location name from TikTok.", {status: 500});
     }
 
+    // Text search Google Places with LLM output
     const placesResult = await searchGooglePlaces(possiblePlaceName);
     if (!placesResult) {
         return new Response("Error searching Google Places.", {status: 500});
@@ -48,8 +52,21 @@ export async function createPost(req: BunRequest): Promise<Response> {
         return new Response("Error finding place ID.", {status: 500});
     }
 
-    
+    // Check if a location with this Google Place ID already exists
+    const [rows, _] = await db.execute("SELECT id FROM map_points WHERE google_place_id = ?", [placeId]) as [any[], any];
+    if (rows.length > 0) {
 
+        // Save post to existing location
+
+        // TODO: Update returned response
+        return new Response();
+    }
+
+    // If the location doesn't exist, we need to create it - first get the details
+    const placeDetails = await getGooglePlaceDetails(placeId);
+    if (!placeDetails) {
+        return new Response("Error fetching Google Place details.", {status: 500});
+    }
 
     // TODO: Update returned response
     return new Response()
