c933a0193fd4ec82ee6a92b4236cb920a0619942
ju-lewis <julianlewis2023@gmail.com>
2025-09-03 22:29:47 +1000

    Update refresh API response

--- api/src/utils.ts
+++ api/src/utils.ts
@@ -29,5 +29,5 @@ export async function checkedExtractBody(
 
 
 export function refresh(_req: BunRequest): Response {
-    return new Response("", {status: 401});
+    return new Response("", {status: 200});
 }
