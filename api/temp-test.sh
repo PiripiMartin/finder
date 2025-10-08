56e46dd8d8fb033b0575f93c49310e5145ec373b
ju-lewis <julianlewis2023@gmail.com>
2025-10-09 10:20:44 +1100

    update testing script

--- api/temp-test.sh
+++ api/temp-test.sh
@@ -1,9 +1,8 @@
 
 # Temporary test API calls
 
-#VID_URL="https://www.tiktok.com/@takemeout_angel/video/7533369493456702727?is_from_webapp=1&sender_device=pc&web_id=7545686590786504210"
-#VID_URL="https://vt.tiktok.com/ZSAG78yvV/"
-VID_URL="https://www.tiktok.com/@gwensies/video/7535377857737002257"
+#VID_URL="https://vt.tiktok.com/ZSULCJ4pr/"
+VID_URL="https://vt.tiktok.com/ZSUduGXs1/"
 
 AUTH_TOKEN="b62ecec6-4c5e-4cb6-afd2-2edf5ef015b5"
 
